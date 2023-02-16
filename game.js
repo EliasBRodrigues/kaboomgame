a0903f860086f91f2ee5a19d29990da9262fa46d
EliasBRodrigues <elias.rborges3@gmail.com>
2023-02-16 16:02:10 -0300

    kaboom

--- game.js
+++ game.js
@@ -1,197 +1,295 @@
-
 kaboom({
-    width: 400,
-    height: 200,
-    stretch: true,
-    letterbox: true,
-    scale: 4,
-    font: "sinko",
+    global: true,
+    fullscreen: true,
+    scale: 2,
+    clearColor: [0,0,0,1]
 })
 
-// Load assets
-loadSprite("mario", "/img/smb_mario_sheet.png", {
-    sliceX: 16,
+let isJumping = true
+let isBig = false
 
+
+loadSprite('bloco', "/img/block-brown.png")
+loadSprite('goomba', '/img/block-brown.png') 
+loadSprite('surpresa', '/img/block-brown.png')
+loadSprite('unboxed', '/img/block-brown.png')
+loadSprite('moeda', '/img/block-brown.png')
+loadSprite('cogumelo', '/img/block-brown.png')
+
+loadSprite("mario", '/img/mario-sheet.png', {
+    sliceX: 6,
     anims: {
-        "idle": {
-            from: 7,
-            to: 8,
+        idle: {
+            from: 0,
+            to: 0,
+        },
+        move: {
+            from: 1,
+            to: 3,
+        },
 
-            speed: 10,
-            loop: true,
-        },
-        "run": {
-            from: 9,
-            to: 12,
-            speed: 10,
-            loop: true,
-        },
-        "jump": 1
+		"jump": 4,
     },
+});
+
+loadSprite('tijolo', '/img/block-brown.png') // tijolo
+loadSprite('tubo-top-left', '/img/block-brown.png') // tubo esquerdo
+loadSprite('tubo-top-right', '/img/block-brown.png') // tubo direito
+loadSprite('tubo-bottom-left', '/img/block-brown.png') // tubo parte de baixo esquerdo
+loadSprite('tubo-bottom-right', '/img/block-brown.png') // tubo parte de baixo direito
+
+loadSprite('blue-bloco', '/img/block-brown.png') // bloco azul
+loadSprite('blue-tijolo', '/img/block-brown.png') // tijolo azul
+loadSprite('blue-aco', '/img/block-brown.png') // bloco de aço azul
+loadSprite('blue-goomba', '/img/block-brown.png')
+
+
+scene("game", ({ level, score, big }) => {
+    layer(["bg", "obj", "ui"], "obj")
+
+    const maps = [
+        [
+            '~                                   ~', 
+            '~                                   ~',
+            '~                                   ~',
+            '~                                   ~',
+            '~                                   ~',
+            '~                                   ~',
+            '~     %   =*=%=                     ~',
+            '~                                   ~',
+            '~                           -+      ~',
+            '~                   ^   ^   ()      ~',
+            '=====================================',
+        ],
+        [       
+            '/                                   /',
+            '/                                   /',
+            '/                                   /',
+            '/                                   /',
+            '/                                   /',
+            '/                                   /',
+            '/    @@@@@@                         /',
+            '/                     x x           /',
+            '/                   x x x x    -+   /',
+            '/          z   z  x x x x x x  ()   /',
+            '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+        ],
+        [
+            '                                     ',
+            '                             !       ',
+            '                            %%%%%%   ',
+            '                     !               ',
+            '             %%    %%%%%             ',
+            '      %%%                            ',
+            '                                     ',
+            '   %                                 ',
+            '=     !    !   =  ^  ^    !    !     ',
+            '===========================    =====/',
+            '                          =    =    /',
+            '                                    /',
+            '        !                           /',
+            '      %*%                           /',
+            ' -+           %                     /',
+            ' ()!         !    ^                 /',
+            '%%%%%%%%%%%%%%%%%%   ===============/',
+            '                                     ',
+            '                                     ',
+            '                                     ',
+          ],
+          [
+            '=                                   =',
+            '=                                   =',
+            '=                                   =',
+            '=                                   =',
+            '=                                   =',
+            '=    ======          =              =',
+            '=                 =  =  =           =',
+            '=              =  =  =  =  =   -+   =',
+            '=              =  =  =  =  =   ()   =',
+            '=====================================',
+          ],
+
+    ]
+
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('bloco'), solid()],
+        '$': [sprite('moeda'), 'moeda'],
+        '%': [sprite('surpresa'), solid(), 'moeda-surpresa'],
+        '*': [sprite('surpresa'), solid(), 'cogumelo-surpresa'],
+        '}': [sprite('unboxed'), solid()],
+        '^': [sprite('goomba'), 'dangerous'],
+        '#': [sprite('cogumelo'), 'cogumelo', body()],
+
+        '~': [sprite('tijolo'), solid()],
+        '(': [sprite('tubo-bottom-left'), solid(), scale(0.5)],
+        ')': [sprite('tubo-bottom-right'), solid(), scale(0.5)],
+        '-': [sprite('tubo-top-left'), solid(), 'tubo', scale(0.5)],
+        '+': [sprite('tubo-top-right'), solid(), 'tubo', scale(0.5)],
+        '!': [sprite('blue-bloco'), solid(), scale(0.5)],
+        '/': [sprite('blue-tijolo'), solid(), scale(0.5)],
+        'z': [sprite('blue-goomba'),body(), 'dangerous', scale(0.5)],
+        'x': [sprite('blue-aco'), solid(), scale(0.5)],
+    }
+
+    const gameLevel = addLevel(maps[level], levelCfg)
+
+    const scoreLabel = add([
+        text('Moedas: ' +score, 10),
+        pos(20,5),
+        layer('ui'),
+        {
+            value: score
+        }
+    ])
+
+    add([text('Level: ' +parseInt(level + 1), 10), pos(20,30)])
+
+    function big(){
+        return{
+            isBig(){
+                return isBig
+            },
+            smallify(){
+                this.scale = vec2(1)
+                isBig = false
+            },
+            biggify(){
+                this.scale = vec2(1.5)
+                isBig = true
+            }
+        }
+    }    
+
+    const player = add([
+        
+        sprite("mario", {
+            animSpeed: 0.1,
+            frame: 0
+        }),
+        solid(),
+        body(),
+        pos(60,0),
+        big(),
+        origin('bot'),
+        {
+            speed: 120
+        }
+    ])
+
+    if(isBig){
+        player.biggify()
+    }
+
+    keyDown('left', () => {
+        player.flipX(true)
+        player.move(-120,0)
+    })
+
+    keyDown('right', () => {
+        player.flipX(false)
+        player.move(120,0)
+    })
+    
+    keyPress('space', () => {
+        if(player.grounded()){
+            player.jump(390)
+            isJumping = true
+        }
+    })
+
+    //animar mario
+    keyPress('left', () => {
+        player.flipX(true)
+        player.play('move')
+    })
+
+    keyPress('right', () => {
+        player.flipX(false)
+        player.play('move')
+    })    
+
+    //////////////////////
+
+    // animar parado //
+    keyRelease('left', () => {
+        player.play('idle')
+    })
+
+    keyRelease('right', () => {
+        player.play('idle')
+    })
+    ///////////////////////////
+
+    action('dangerous', (obj) => {
+        obj.move(-20,0)
+    })
+
+    action('cogumelo', (obj) => {
+        obj.move(20,0)
+    })
+
+    player.action(() => {
+        if(player.grounded()){
+            isJumping = false
+        }
+    })
+
+    player.on('headbutt', (obj) => {
+        if(obj.is('moeda-surpresa')){
+            gameLevel.spawn('$', obj.gridPos.sub(0,1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0,0))
+        }
+
+        if(obj.is('cogumelo-surpresa')){
+            gameLevel.spawn('#', obj.gridPos.sub(0,1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0,0))
+        }
+
+    })
+
+    player.collides('cogumelo', (obj) => {
+        destroy(obj)
+        player.biggify()
+    })
+
+    player.collides('dangerous', (obj) => {
+        if(isJumping){
+            destroy(obj)
+        }else{
+            if(isBig){
+                player.smallify()
+            }else{
+                go("lose", ({score: scoreLabel.value}))
+            }
+        }
+    })
+
+    player.collides('moeda', (obj) => {
+        destroy(obj)
+        scoreLabel.value++
+        scoreLabel.text = 'Moedas: ' +scoreLabel.value
+    })
+
+    player.collides('tubo', () => {
+        keyPress('down', () => {
+            go("game", {
+                level: (level + 1) % maps.length,
+                score: scoreLabel.value,
+                big: isBig
+            })
+        })
+    })
 })
 
-const SPEED = 120
-const JUMP_FORCE = 240
-
-gravity(640)
-
-// Add our player character
-const player = add([
-	sprite("mario"),
-	pos(center()),
-	origin("center"),
-	area(),
-	body(),
-])
-
-// .play is provided by sprite() component, it starts playing the specified animation (the animation information of "idle" is defined above in loadSprite)
-player.play("idle")
-
-// Add a platform
-add([
-	rect(width(), 24),
-	area(),
-	outline(1),
-	pos(0, height() - 24),
-	solid(),
-])
-
-// Switch to "idle" or "run" animation when player hits ground
-player.onGround(() => {
-	if (!isKeyDown("left") && !isKeyDown("right")) {
-		player.play("idle")
-	} else {
-		player.play("run")
-	}
+scene("lose", ({score}) => {
+    add([ text('Score: ' +score, 18), origin('center'), pos(width()/2, height()/2) ])
+    keyPress('space', () => {
+        go("game", {level: 0, score: 0, big: isBig})
+    })
 })
 
-player.onAnimEnd("idle", () => {
-	// You can also register an event that runs when certain anim ends
-})
-
-onKeyPress("space", () => {
-	if (player.isGrounded()) {
-		player.jump(JUMP_FORCE)
-		player.play("jump")
-	}
-})
-
-onKeyDown("left", () => {
-	player.move(-SPEED, 0)
-	player.flipX(true)
-	// .play() will reset to the first frame of the anim, so we want to make sure it only runs when the current animation is not "run"
-	if (player.isGrounded() && player.curAnim() !== "run") {
-		player.play("run")
-	}
-})
-
-onKeyDown("right", () => {
-	player.move(SPEED, 0)
-	player.flipX(false)
-	if (player.isGrounded() && player.curAnim() !== "run") {
-		player.play("run")
-	}
-})
-
-onKeyRelease(["left", "right"], () => {
-	// Only reset to "idle" if player is not holding any of these keys
-	if (player.isGrounded() && !isKeyDown("left") && !isKeyDown("right")) {
-		player.play("idle")
-	}
-})
-
-const getInfo = () => `
-Anim: ${player.curAnim()}
-Frame: ${player.frame}
-`.trim()
-
-// Add some text to show the current animation
-const label = add([
-	text(getInfo()),
-	pos(4),
-])
-
-label.onUpdate(() => {
-	label.text = getInfo()
-})
-
-
-// loadSprite("enemy", "/img/enemy.png")
-// loadSprite("block", "/img/block.png")
-// loadSprite("coin", "/img/coin.png")
-
-// // Define player movement speed (pixels per second)
-// const SPEED = 480
-
-// gravity(2400)
-
-// const level = addLevel([
-//     "@  ^  $$",
-//     "================",
-// ], {
-//     width: 40,
-//     height: 50,
-
-//     pos: vec2(200, 500),
-
-//     "@": () => [
-//         sprite("bean"),
-//         area(),
-//         body(),
-//         origin("bot"),
-//         "player",
-//     ],
-//     "=": () => [
-// 		sprite("block"),
-// 		area(),
-// 		solid(),
-// 		origin("bot"),
-// 	],
-// 	"$": () => [
-// 		sprite("coin"),
-// 		area(),
-// 		origin("bot"),
-// 		"coin",
-// 	],
-// 	"^": () => [
-// 		sprite("enemy"),
-// 		area(),
-// 		origin("bot"),
-// 		"danger",
-// 	],
-// })
-
-
-
-// // Add player game object
-// const player = get("player")[0]
-
-// onKeyPress("space", () => {
-//     if(player.isGrounded()){
-//         player.jump()
-//     }
-// })
-
-// // onKeyDown() registers an event that runs every frame as long as user is holding a certain key
-// onKeyDown("left", () => {
-// 	// .move() is provided by pos() component, move by pixels per second
-// 	player.move(-SPEED, 0)
-// })
-
-// onKeyDown("right", () => {
-// 	player.move(SPEED, 0)
-// })
-
-
-
-// player.onCollide("danger", () => {
-//     player.pos = level.getPos(0, 0)
-// })
-
-
-// player.onCollide("coin", (coin) => {
-//    destroy(coin)
-// })
-
-
-
+go("game", ({ level: 0, score: 0, big: isBig }))
